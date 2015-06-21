81be21228a8e7ed2cffd3de884cd3541e05056d4
Derek Cacciotti <derekcacciotti@Dereks-iMac.local>
2015-06-21 00:03:39 -0400

    made a single pipe appear

--- EndlessFlappy/GameScene.swift
+++ EndlessFlappy/GameScene.swift
@@ -9,37 +9,80 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    
+    var mainPipe: SKSpriteNode = SKSpriteNode()
+    
+    var spacebetweenpipes:Float = 90 // number of pixels between each space
+    
+    var pipes:[SKSpriteNode] = []
+    
+    override func didMoveToView(view: SKView)
+    {
+        mainPipe = SKSpriteNode(color: UIColor.blackColor(), size: CGSizeMake(view.bounds.size.width / 6, 480))
         
-        self.addChild(myLabel)
+        
+        
+        // put pipes on screen
+        createPipes(0)
     }
+    
+    func createPipes(offs:Float)
+    {
+       let offset = offs - (spacebetweenpipes / 2)
+        let pipebottom = (mainPipe as SKSpriteNode).copy() as! SKSpriteNode
+        let pipeTop = (mainPipe as SKSpriteNode).copy() as! SKSpriteNode
+        
+        let xx = Float(view!.bounds.size.width) // casted to a float
+        
+       self.setBottomPosition(pipebottom, x: xx, y: offset)
+        self.setTopPosition(pipeTop, x: xx, y: offset + spacebetweenpipes)
+        
+        // adding pipes to pipes array so they can be added as the scene moves along
+        pipes.append(pipebottom)
+        pipes.append(pipeTop)
+        
+        
+        self.addChild(pipebottom)
+        self.addChild(pipeTop)
+        
+        
+        
+        
+    }
+    
+    func setBottomPosition(node:SKSpriteNode, x:Float, y:Float)
+    {
+        let xpos = (Float(node.size.width) / 2) + x
+        let ypos = Float(self.view!.bounds.size.height) / 2 - (Float(node.size.height) / 2) + y
+        
+        node.position.x = CGFloat(xpos)
+        node.position.y = CGFloat(ypos)
+    }
+    
+    func setTopPosition(node:SKSpriteNode,x:Float, y:Float)
+    {
+        let xx = (Float(node.size.width) / 2) + x
+        let yy = Float(self.view!.bounds.size.height) / 2 + (Float(node.size.height) / 2) + y
+        
+        node.position.x = CGFloat(xx)
+        node.position.y = CGFloat(yy)
+    }
+    
+    
+    
+    
+    
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
-    }
    
     override func update(currentTime: CFTimeInterval) {
+        for (var i = 0; i < pipes.count; i++)
+        {
+            let pipe = pipes[i]
+        }
         /* Called before each frame is rendered */
     }
 }
